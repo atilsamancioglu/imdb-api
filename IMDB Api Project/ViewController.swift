49bdc2e58cfa8862d79b32c51414cc4bbf9611b9
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-24 21:00:35 +0300

    final version

--- IMDB Api Project/ViewController.swift
+++ IMDB Api Project/ViewController.swift
@@ -8,18 +8,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var actorsLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        searchBar.delegate = self
+        
+        self.movieImage.layer.cornerRadius = self.movieImage.frame.size.width / 2
+        self.movieImage.clipsToBounds = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchIMDB(title: searchBar.text!)
+        searchBar.text = ""
+        
     }
+    
+    func searchIMDB(title:String) {
+        
+        if let movie = title.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed){
+            
+            let url = URL(string: "http://www.omdbapi.com/?t=\(movie)")
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: url!, completionHandler: { (data, response, error) in
+                
+                if error != nil {
+                    print(error?.localizedDescription)
+                
+                } else {
+                    
+                    
+                    if data != nil {
+                        
+                        do
+                            
+                        {
+                            let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String,String>
+                            DispatchQueue.main.async{
+                                
+                                self.titleLabel.text = jsonResult["Title"]
+                                self.ratingLabel.text = jsonResult["imdbRating"]
+                                self.directorLabel.text = jsonResult["Director"]
+                                self.actorsLabel.text = jsonResult["Actors"]
+                                
+                                if let posterExists = jsonResult["Poster"]{
+                                    
+                                    let posterUrl = URL(string:posterExists)
+                                    if let posterData = try? Data(contentsOf: posterUrl!) {
+                                        
+                                        self.movieImage.image = UIImage(data:posterData)
+                                    
+                                    }
+                                    
 
+                                }
+                                
+                            }
+                            
+                            
+                        } catch {
+                            
+                        }
 
+                    }
+                    
+                    
+                }
+                
+                
+            })
+            
+             task.resume()
+            
+        }
+    }
+    
+    
 }
 
